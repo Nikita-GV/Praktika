45c05c1cecdcf08d7e4cc74f058077d80772da15
Nikita-GV <64585333+Nikita-GV@users.noreply.github.com>
2022-04-07 11:19:12 +0300

    Add files via upload

---
+++ prakt/prakt/prakt/prakt/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace prakt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(47, 79);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(132, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 42);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(295, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Введите колличество элементов в массиве";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(571, 42);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(367, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Исходный массив (не должен превышать 20 элементов)";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(47, 133);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 44);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Сортировка пузырьком";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(216, 133);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(133, 44);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Сортировка вставками ";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(571, 337);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(180, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Отсортированный массив ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(216, 86);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(10, 16);
+            this.label4.TabIndex = 6;
+            this.label4.Text = ".";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1236, 655);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Гаврилов Н.К. Гулин А.В.";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ prakt/prakt/prakt/prakt/Form1.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prakt
+{
+    public partial class Form1 : Form
+    {
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (textBox1.Text == "")
+            {
+                label4.Text = "Введите колличество элементов в массиве"; //Проверка на заполнение
+            }
+            else { 
+
+            string st = "Исходный массив (не должен превышать 20 элементов) ";
+            string st2 = "Отсортированный массив ";
+            label2.Text = st;
+            label3.Text = st2;
+            int N;
+                if (int.TryParse(textBox1.Text, out int number))      //проверка на число
+                {
+
+
+
+                    N = Int32.Parse(textBox1.Text);
+                    if (N > 20)
+                    {
+                        label4.Text = "Количество элементов массива не должно превышать 20";
+                    }
+                    else
+                    {
+
+
+                        if (N == 1)
+                        {
+                            label4.Text = "Нельзя сортировать массив из одного элемента";
+                        }
+                        else
+                        {
+                            if (N == 0)
+                            {
+                                label4.Text = "Количество элементов массива не может быть равно 0";
+                            }
+                            else
+                            {
+                                if (N < 0)
+                                {
+                                    label4.Text = "Введите положительное колличество элементов массива"; //Проверка на положительное число элементов
+                                }
+                                else
+                                {
+                                    int[] mas = new int[N];
+                                    Random rand = new Random();
+                                    for (int i = 0; i < mas.Length; i++)
+                                    {
+                                        mas[i] = rand.Next(1, 21);              //заполнение массива
+                                    }
+
+                                    Console.WriteLine("До сортировки:");
+                                    for (int i = 0; i < mas.Length; i++)
+                                    {
+
+                                        label2.Text += (mas[i]).ToString();             //вывод массива до сортировки
+                                        label2.Text += " ";
+                                    }
+
+
+                                    int temp;
+                                    for (int i = 0; i < mas.Length; i++)
+                                    {
+                                        for (int j = i + 1; j < mas.Length; j++)
+                                        {
+                                            if (mas[i] > mas[j])
+                                            {
+                                                temp = mas[i];
+                                                mas[i] = mas[j];                          //сортировка
+                                                mas[j] = temp;
+                                            }
+                                        }
+                                    }
+
+                                    Console.WriteLine("После сортировки:");
+                                    for (int i = 0; i < mas.Length; i++)
+                                    {
+                                        label3.Text += (mas[i]).ToString();             //вывод массива после
+                                        label3.Text += " ";
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    label4.Text = "Введите число";
+                }
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+
+            if (textBox1.Text == "")
+            {
+                label4.Text = "Введите колличество элементов в массиве"; //Проверка на заполнение
+            }
+            else
+            {
+
+
+                string st = "Исходный массив (не должен превышать 20 элементов) ";
+                string st2 = "Отсортированный массив ";
+                label2.Text = st;
+                label3.Text = st2;
+                int N;
+                if (int.TryParse(textBox1.Text, out int number))        //проверка на число
+                {
+
+
+
+                    N = Int32.Parse(textBox1.Text);
+                    if (N > 20)
+                    {
+                        label4.Text = "Количество элементов массива не должно превышать 20";
+                    }
+                    else
+                    {
+                        if (N == 1)
+                        {
+                            label4.Text = "Нельзя сортировать массив из одного элемента";
+                        }
+                        else
+                        {
+                            if (N == 0)
+                            {
+                                label4.Text = "Количество элементов массива не может быть равно 0";
+                            }
+                            else
+                            {
+                                if (N < 0)
+                                {
+                                    label4.Text = "Введите положительное колличество элементов массива"; //Проверка на положительное число элементов
+                                }
+                                else
+                                {
+                                    int[] mas = new int[N];
+                                    Random rand = new Random();
+                                    for (int i = 0; i < mas.Length; i++)
+                                    {
+                                        mas[i] = rand.Next(1, 21);              //заполнение массива
+                                    }
+
+                                    Console.WriteLine("До сортировки:");
+                                    for (int i = 0; i < mas.Length; i++)
+                                    {
+
+                                        label2.Text += (mas[i]).ToString();             //вывод массива до сортировки
+                                        label2.Text += " ";
+                                    }
+
+                                    int index;                             //номер элемента в массиве
+                                    int currentNumber;                     //значение текущего элемента в массиве
+                                    for (int i = 0; i < mas.Length; i++)
+                                    {
+                                        index = i;
+                                        currentNumber = mas[i];
+                                        while (index > 0 && currentNumber < mas[index - 1])
+                                        {
+                                            mas[index] = mas[index - 1];
+                                            index--;
+                                        }
+                                        mas[index] = currentNumber;
+                                    }
+
+                                    for (int i = 0; i < mas.Length; i++)
+                                    {
+                                        label3.Text += (mas[i]).ToString();             //вывод массива после
+                                        label3.Text += " ";
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    label4.Text = "Введите число";
+                }
+            }
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
